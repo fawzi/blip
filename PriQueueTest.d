47b9de21dc2027f1d6209aa368515a7277c7384a
Fawzi Mohamed <fawzi@gmx.ch>
2008-11-03 13:43:55 +0100

    first working version of SMP parallelization framework This needs some patches to the current tango (stresses Thread, Fibers and GC), and is quite verbose. To disable parallelization use -version=SequentialWorkManager. It should not even use fibers in the normal case. The code is quite verbose at the moment, this will go away soon.

---
+++ PriQueueTest.d
@@ -0,0 +1,23 @@
+module PriQueueTest;
+import blip.parallel.WorkManager;
+import tango.io.Stdout;
+
+void main(){
+    PriQueue!(Task) queue=new PriQueue!(Task)();
+    queue.insert(10,new Task("bla1", delegate(){Stdout("task1").newline;}));
+    queue.desc(Stdout("xx1")).newline;
+    auto t=queue.popNext();
+    queue.desc(Stdout("xx2")).newline;
+    queue.insert(12,new Task("bla2", delegate(){Stdout("task2").newline;}));
+    queue.desc(Stdout("xx3")).newline;
+    queue.insert(10,t);
+    queue.desc(Stdout("xx4")).newline;
+    queue.insert(12,t);
+    queue.desc(Stdout("xx5")).newline;
+    auto t2=queue.popNext();
+    queue.desc(Stdout("xx5.1")).newline;
+    queue.insert(20,t);    
+    queue.desc(Stdout("xx6")).newline;
+    queue.insert(0,t);    
+    queue.desc(Stdout("xx7")).newline;
+}
