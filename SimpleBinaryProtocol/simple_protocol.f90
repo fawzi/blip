85e6fb3e9830f29761534de4cb50088adc0b7d9d
Fawzi Mohamed <fawzi@gmx.ch>
2009-09-22 23:46:13 +0200

    improving project file, improving fortran interface

--- SimpleBinaryProtocol/simple_protocol.f90
+++ SimpleBinaryProtocol/simple_protocol.f90
@@ -1,3 +1,5 @@
+module simple_protocol
+public
 !! a simple binary protocol
 !! kind,size,msg in network order
 !! useful to connect fortran programs
@@ -11,13 +13,11 @@
 
 ! for a slightly better description look at the C header descriptions
 
-! one could overload 32 and 64 bit versions, to make the usage simpler
-
 INTEGER, PARAMETER :: sbp_real_8 = SELECTED_REAL_KIND ( 14, 200 )
 INTEGER, PARAMETER :: sbp_int_8 = SELECTED_INT_KIND(18)
 INTEGER, PARAMETER :: sbp_int_4 = SELECTED_INT_KIND(6)
 
-INTEGER(int_4), PARAMETER :: sbp_kind_raw=0, &
+INTEGER(SELECTED_INT_KIND(6)), PARAMETER :: sbp_kind_raw=0, &
     sbp_kind_char=1, &
     sbp_kind_int_small=2, &
     sbp_kind_double_small=3
@@ -26,12 +26,12 @@ INTEGER(int_4), PARAMETER :: sbp_kind_raw=0, &
 
 INTERFACE
    SUBROUTINE sbpinit(ierr)
-    INTEGER(sbp_int_4) :: ierr
+    INTEGER(selected_int_kind(6)) :: ierr
    END SUBROUTINE
 END INTERFACE
 INTERFACE
    SUBROUTINE sbpend(ierr)
-    INTEGER(sbp_int_4) :: ierr
+    INTEGER(selected_int_kind(6)) :: ierr
    END SUBROUTINE
 END INTERFACE
 
@@ -39,213 +39,253 @@ END INTERFACE
 
 INTERFACE
    SUBROUTINE sbpsendh64(ierr,sock,kind,len)
-    INTEGER(sbp_int_4) :: ierr,sock,kind
-    INTEGER(sbp_int_8) :: len
+    INTEGER(selected_int_kind(6)) :: ierr,sock,kind
+    INTEGER(selected_int_kind(18)) :: len
    END SUBROUTINE
 END INTERFACE
 INTERFACE
    SUBROUTINE sbpsendh32(ierr,sock,kind,len)
-    INTEGER(sbp_int_4) :: ierr,sock,kind,len
+    INTEGER(selected_int_kind(6)) :: ierr,sock,kind,len
    END SUBROUTINE
+END INTERFACE
+
+INTERFACE sbpsendh
+   MODULE PROCEDURE sbpsendh64, sbpsendh32
 END INTERFACE
 
 INTERFACE
    SUBROUTINE sbpsendc32n(ierr,sock,str)
-    INTEGER(sbp_int_4) :: ierr,sock
+    INTEGER(selected_int_kind(6)) :: ierr,sock
     CHARACTER(LEN=*) :: str
    END SUBROUTINE
 END INTERFACE
 INTERFACE
    SUBROUTINE sbpsendc64n(ierr,sock,str)
-    INTEGER(sbp_int_4) :: ierr,sock
+    INTEGER(selected_int_kind(6)) :: ierr,sock
     CHARACTER(LEN=*) :: str
    END SUBROUTINE
 END INTERFACE
 
 INTERFACE
    SUBROUTINE sbpsendi64(ierr,sock,iarr,len)
-    INTEGER(sbp_int_4) :: ierr,sock,len
-    INTEGER(sbp_int_4), dimension(*) :: iarr
+    INTEGER(selected_int_kind(6)) :: ierr,sock
+    INTEGER(selected_int_kind(18)) :: len
+    INTEGER(selected_int_kind(6)), dimension(*) :: iarr
    END SUBROUTINE
 END INTERFACE
 INTERFACE
-   SUBROUTINE sbpsendi32(ierr,sock,str,len)
-    INTEGER(sbp_int_4) :: ierr,sock,len
-    INTEGER(LEN=*) :: str
+   SUBROUTINE sbpsendi32(ierr,sock,iarr,len)
+    INTEGER(selected_int_kind(6)) :: ierr,sock,len
+    INTEGER(selected_int_kind(6)), dimension(:) :: iarr
    END SUBROUTINE
 END INTERFACE
+INTERFACE sbpsendi
+    MODULE PROCEDURE sbpsendi64,sbpsendi32
+END INTERFACE
+
 INTERFACE
    SUBROUTINE sbpsendip64(ierr,sock,iarr,len)
-    INTEGER(sbp_int_4) :: ierr,sock
-    INTEGER(sbp_int_8) :: len
-    INTEGER(sbp_int_4), dimension(*) :: iarr
+    INTEGER(selected_int_kind(6)) :: ierr,sock
+    INTEGER(selected_int_kind(18)) :: len
+    INTEGER(selected_int_kind(6)), dimension(*) :: iarr
    END SUBROUTINE
 END INTERFACE
 INTERFACE
-   SUBROUTINE sbpsendip32(ierr,sock,str,len)
-    INTEGER(sbp_int_4) :: ierr,sock,len
-    INTEGER(sbp_int_4), dimension(*) :: iarr
+   SUBROUTINE sbpsendip32(ierr,sock,iarr,len)
+    INTEGER(selected_int_kind(6)) :: ierr,sock,len
+    INTEGER(selected_int_kind(6)), dimension(*) :: iarr
    END SUBROUTINE
+END INTERFACE
+INTERFACE sbpsendip
+    MODULE PROCEDURE sbpsendip64,sbpsendip32
 END INTERFACE
 
 INTERFACE
    SUBROUTINE sbpsendd64(ierr,sock,arr,len)
-    INTEGER(sbp_int_4) :: ierr,sock,len
-    REAL(sbp_real_8), dimension(*) :: arr
+    INTEGER(selected_int_kind(6)) :: ierr,sock
+    INTEGER(selected_int_kind(18)) :: len
+    REAL(selected_real_kind(14,200)), dimension(*) :: arr
    END SUBROUTINE
 END INTERFACE
 INTERFACE
-   SUBROUTINE sbpsendd32(ierr,sock,str,len)
-    INTEGER(sbp_int_4) :: ierr,sock,len
-    REAL(sbp_real_8), dimension(*) :: arr
+   SUBROUTINE sbpsendd32(ierr,sock,arr,len)
+    INTEGER(selected_int_kind(6)) :: ierr,sock,len
+    REAL(selected_real_kind(14,200)), dimension(*) :: arr
    END SUBROUTINE
 END INTERFACE
+INTERFACE sbpsendd
+    MODULE PROCEDURE sbpsendd64,sbpsendd32
+END INTERFACE
+
 INTERFACE
    SUBROUTINE sbpsenddp64(ierr,sock,arr,len)
-    INTEGER(sbp_int_4) :: ierr,sock
-    INTEGER(sbp_int_8) :: len
-    REAL(sbp_real_8), dimension(*) :: arr
+    INTEGER(selected_int_kind(6)) :: ierr,sock
+    INTEGER(selected_int_kind(18)) :: len
+    REAL(selected_real_kind(14,200)), dimension(*) :: arr
    END SUBROUTINE
 END INTERFACE
 INTERFACE
    SUBROUTINE sbpsenddp32(ierr,sock,arr,len)
-    INTEGER(sbp_int_4) :: ierr,sock,len
-    REAL(sbp_real_8), dimension(*) :: arr
+    INTEGER(selected_int_kind(6)) :: ierr,sock,len
+    REAL(selected_real_kind(14,200)), dimension(*) :: arr
    END SUBROUTINE
+END INTERFACE
+INTERFACE sbpsenddp
+    MODULE PROCEDURE sbpsenddp64,sbpsenddp32
 END INTERFACE
 
 !!!!!!!!!!! receiving !!!!!!!!!!!
 
 INTERFACE
    SUBROUTINE sbpreadh64(ierr,sock,kind,len)
-    INTEGER(sbp_int_4) :: ierr,sock,kind
-    INTEGER(sbp_int_8) :: len
+    INTEGER(selected_int_kind(6)) :: ierr,sock,kind
+    INTEGER(selected_int_kind(18)) :: len
    END SUBROUTINE
 END INTERFACE
 INTERFACE
    SUBROUTINE sbpreadh32(ierr,sock,kind,len)
-    INTEGER(sbp_int_4) :: ierr,sock,kind,len
+    INTEGER(selected_int_kind(6)) :: ierr,sock,kind,len
    END SUBROUTINE
+END INTERFACE
+INTERFACE sbpreadh
+    MODULE PROCEDURE sbpreadh64,sbpreadh32
 END INTERFACE
 
 INTERFACE
    SUBROUTINE sbpreadc32n(ierr,sock,str)
-    INTEGER(sbp_int_4) :: ierr,sock
+    INTEGER(selected_int_kind(6)) :: ierr,sock
     CHARACTER(LEN=*) :: str
    END SUBROUTINE
 END INTERFACE
 ! probably wrong (I know no compiler that uses 64 bit int for string length)
 INTERFACE
    SUBROUTINE sbpreadc64n(ierr,sock,str)
-    INTEGER(sbp_int_4) :: ierr,sock
+    INTEGER(selected_int_kind(6)) :: ierr,sock
     CHARACTER(LEN=*) :: str
    END SUBROUTINE
 END INTERFACE
+
 INTERFACE
    SUBROUTINE sbpreadcp32n(ierr,sock,str)
-    INTEGER(sbp_int_4) :: ierr,sock
+    INTEGER(selected_int_kind(6)) :: ierr,sock
     CHARACTER(LEN=*) :: str
    END SUBROUTINE
 END INTERFACE
 ! probably wrong (I know no compiler that uses 64 bit int for string length)
 INTERFACE
    SUBROUTINE sbpreadcp64n(ierr,sock,str)
-    INTEGER(sbp_int_4) :: ierr,sock
+    INTEGER(selected_int_kind(6)) :: ierr,sock
     CHARACTER(LEN=*) :: str
    END SUBROUTINE
 END INTERFACE
 
 INTERFACE
    SUBROUTINE sbpreadi64(ierr,sock,iarr,len)
-    INTEGER(sbp_int_4) :: ierr,sock,len
-    INTEGER(sbp_int_4), dimension(*) :: iarr
+    INTEGER(selected_int_kind(6)) :: ierr,sock
+    INTEGER(selected_int_kind(6)), dimension(*) :: iarr
+    INTEGER(selected_int_kind(18)) :: len
    END SUBROUTINE
 END INTERFACE
 INTERFACE
-   SUBROUTINE sbpreadi32(ierr,sock,str,len)
-    INTEGER(sbp_int_4) :: ierr,sock,len
-    INTEGER(LEN=*) :: str
+   SUBROUTINE sbpreadi32(ierr,sock,iarr,len)
+    INTEGER(selected_int_kind(6)) :: ierr,sock,len
+    INTEGER(selected_int_kind(6)),dimension(:) :: iarr
    END SUBROUTINE
 END INTERFACE
+INTERFACE sbpreadi
+    MODULE PROCEDURE sbpreadi64, sbpreadi32
+END INTERFACE
+
 INTERFACE
    SUBROUTINE sbpreadip64(ierr,sock,iarr,len)
-    INTEGER(sbp_int_4) :: ierr,sock
-    INTEGER(sbp_int_8) :: len
-    INTEGER(sbp_int_4), dimension(*) :: iarr
+    INTEGER(selected_int_kind(6)) :: ierr,sock
+    INTEGER(selected_int_kind(18)) :: len
+    INTEGER(selected_int_kind(6)), dimension(*) :: iarr
    END SUBROUTINE
 END INTERFACE
 INTERFACE
-   SUBROUTINE sbpreadip32(ierr,sock,str,len)
-    INTEGER(sbp_int_4) :: ierr,sock,len
-    INTEGER(sbp_int_4), dimension(*) :: iarr
+   SUBROUTINE sbpreadip32(ierr,sock,iarr,len)
+    INTEGER(selected_int_kind(6)) :: ierr,sock,len
+    INTEGER(selected_int_kind(6)), dimension(*) :: iarr
    END SUBROUTINE
+END INTERFACE
+INTERFACE sbpreadip
+    MODULE PROCEDURE sbpreadip64, sbpreadip32
 END INTERFACE
 
 INTERFACE
    SUBROUTINE sbpreadd64(ierr,sock,arr,len)
-    INTEGER(sbp_int_4) :: ierr,sock,len
-    REAL(sbp_real_8), dimension(*) :: arr
+    INTEGER(selected_int_kind(6)) :: ierr,sock
+    INTEGER(selected_int_kind(18)) :: len
+    REAL(selected_real_kind(14,200)), dimension(*) :: arr
    END SUBROUTINE
 END INTERFACE
 INTERFACE
-   SUBROUTINE sbpreadd32(ierr,sock,str,len)
-    INTEGER(sbp_int_4) :: ierr,sock,len
-    REAL(sbp_real_8), dimension(*) :: arr
+   SUBROUTINE sbpreadd32(ierr,sock,arr,len)
+    INTEGER(selected_int_kind(6)) :: ierr,sock,len
+    REAL(selected_real_kind(14,200)), dimension(*) :: arr
    END SUBROUTINE
 END INTERFACE
+INTERFACE sbpreadd
+    MODULE PROCEDURE sbpreadd64, sbpreadd32
+END INTERFACE
+
 INTERFACE
    SUBROUTINE sbpreaddp64(ierr,sock,arr,len)
-    INTEGER(sbp_int_4) :: ierr,sock
-    INTEGER(sbp_int_8) :: len
-    REAL(sbp_real_8), dimension(*) :: arr
+    INTEGER(selected_int_kind(6)) :: ierr,sock
+    INTEGER(selected_int_kind(18)) :: len
+    REAL(selected_real_kind(14,200)), dimension(*) :: arr
    END SUBROUTINE
 END INTERFACE
 INTERFACE
    SUBROUTINE sbpreaddp32(ierr,sock,arr,len)
-    INTEGER(sbp_int_4) :: ierr,sock,len
-    REAL(sbp_real_8), dimension(*) :: arr
+    INTEGER(selected_int_kind(6)) :: ierr,sock,len
+    REAL(selected_real_kind(14,200)), dimension(*) :: arr
    END SUBROUTINE
 END INTERFACE
+INTERFACE sbpreaddp
+    MODULE PROCEDURE sbpreaddp64, sbpreaddp32
+END INTERFACE
 
-/// connection
+!!! connection
 INTERFACE
    SUBROUTINE sboconn32n(ierr,sock,str)
-    INTEGER(sbp_int_4) :: ierr,sock
+    INTEGER(selected_int_kind(6)) :: ierr,sock
     CHARACTER(len=*) :: str
    END SUBROUTINE
 END INTERFACE
 INTERFACE
    SUBROUTINE sboconn64n(ierr,sock,str)
-    INTEGER(sbp_int_4) :: ierr,sock
+    INTEGER(selected_int_kind(6)) :: ierr,sock
     CHARACTER(len=*) :: str
    END SUBROUTINE
 END INTERFACE
 INTERFACE
    SUBROUTINE sbplisten32n(ierr,sock,portStr)
-    INTEGER(sbp_int_4) :: ierr,sock
+    INTEGER(selected_int_kind(6)) :: ierr,sock
     CHARACTER(len=*) :: portStr
    END SUBROUTINE
 END INTERFACE
 INTERFACE
    SUBROUTINE sbplisten64n(ierr,sock,portStr)
-    INTEGER(sbp_int_4) :: ierr,sock
+    INTEGER(selected_int_kind(6)) :: ierr,sock
     CHARACTER(len=*) :: portStr
    END SUBROUTINE
 END INTERFACE
 INTERFACE
    SUBROUTINE sbpclose(ierr,sock)
-    INTEGER(sbp_int_4) :: ierr,sock
+    INTEGER(selected_int_kind(6)) :: ierr,sock
    END SUBROUTINE
 END INTERFACE
 INTERFACE
    SUBROUTINE sbpaccept32n(ierr,sock, newSock,addr)
-    INTEGER(sbp_int_4) :: ierr,sock, newSock
+    INTEGER(selected_int_kind(6)) :: ierr,sock, newSock
     CHARACTER(len=*) :: addr
    END SUBROUTINE
 END INTERFACE
 INTERFACE
    SUBROUTINE sbpaccept64n(ierr,sock, newSock,addr)
-    INTEGER(sbp_int_4) :: ierr,sock, newSock
+    INTEGER(selected_int_kind(6)) :: ierr,sock, newSock
     CHARACTER(len=*) :: addr
    END SUBROUTINE
 END INTERFACE
@@ -259,3 +299,5 @@ INTERFACE
     CHARACTER(len=*) :: addr
    END SUBROUTINE
 END INTERFACE
+
+end module
