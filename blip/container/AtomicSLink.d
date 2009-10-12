ca635321d8b570ecb4dbe83f52318669b988ae32
Fawzi Mohamed <fawzi@gmx.ch>
2009-10-12 11:22:29 +0200

    * adding forgotten atomic single linked list

---
+++ blip/container/AtomicSLink.d
@@ -0,0 +1,20 @@
+/// atomic singly linked list ops
+/// at the moment it works on any type that builds a liked list using a field called next
+/// T is supposed to be a pointer to an element of the linked list.
+///
+/// Make this also as template with user given next field? would need mixins...
+///
+/// author: fawzi
+/// license: apache 2.0
+module blip.container.AtomicSLink;
+import tango.core.sync.Atomic: atomicOp;
+
+/// inserts newHead before head, and returns the value at head when the insertion took place
+T insertAt(T)(ref T head,T newHead){
+    return atomicOp(head,delegate T(T val){ newHead.next=val; return newHead; });
+}
+
+/// removes one element from the top of list
+T popFrom(T)(ref T list){
+    return atomicOp(list,delegate T(T val){ return ((val is null)?null:val.next); });
+}
