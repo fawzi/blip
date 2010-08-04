9d46cb80a91a1da47968ba9961ff399447b10a0f
Fawzi Mohamed <fawzi@gmx.ch>
2010-08-04 15:54:43 +0200

    helper scripts to compile the tango trunk

---
+++ buildTango/buildDmdOsx.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+TANGO_HOME=../../tango
+if [ $# -gt 0 ]; then
+    TANGO_HOME=$1
+fi
+$TANGO_HOME/build/bin/osx32/bob -r=dmd -c=dmd -p=osx -l=libtango-base-dmd -o="-O -release -inline"  $TANGO_HOME
+$TANGO_HOME/build/bin/osx32/bob -u -c=dmd -p=osx -l=libtango-user-dmd -o="-g -O -release -inline"  $TANGO_HOME
+$TANGO_HOME/build/bin/osx32/bob -u -c=dmd -p=osx -l=libtango-user-dmd-dbg -o="-g -O -inline"  $TANGO_HOME
+$TANGO_HOME/build/bin/osx32/bob -u -c=dmd -p=osx -l=libtango-user-dmd-tst -o="-d -version=UnitTest -debug=UnitTest -unittest -g -debug -L-ltango-dbg"  $TANGO_HOME
+rm *.o
+dirDmd="`which dmd`"
+dirDmd=`dirname "$dirDmd"`
+mv libtango-base-dmd.a libtango-user-dmd*.a "$dirDmd/../lib"
+echo built and installed libs
+echo creating regtest...
+dmd -d -version=UnitTest -debug=UnitTest -unittest -g -debug -L-ltango-user-dmd-tst -L-lz -L-lbz2 runUnittests_dmd.d
+rm *.o
+./runUnittests_dmd
---
+++ buildTango/buildLdc.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+TANGO_HOME=../../tango
+if [ $# -gt 0 ]; then
+    TANGO_HOME=$1
+fi
+$TANGO_HOME/build/bin/linux64/bob -r=ldc -c=ldc -p=linux -l=libtango-base-ldc -o="-O3 -release -inline"  $TANGO_HOME
+$TANGO_HOME/build/bin/linux64/bob -u -c=ldc -p=linux -l=libtango-user-ldc -o="-g -O3 -release -inline"  $TANGO_HOME
+$TANGO_HOME/build/bin/linux64/bob -u -c=ldc -p=linux -l=libtango-user-ldc-dbg -o="-g -O -inline"  $TANGO_HOME
+$TANGO_HOME/build/bin/linux64/bob -u -c=ldc -p=linux -l=libtango-user-ldc-tst -o="-d -version=UnitTest -debug=UnitTest -unittest -g -debug -L-ltango-dbg"  $TANGO_HOME
+rm *.o
+dirldc="`which ldc`"
+dirldc=`dirname "$dirldc"`
+mv libtango-base-ldc.a libtango-user-ldc*.a "$dirldc/../lib"
+echo built and installed libs
+echo creating regtest
+ldc -d -version=UnitTest -debug=UnitTest -unittest -g -debug -L-ltango-user-ldc-tst -L-lz -L-lbz2 runUnittests_ldc.d
+rm *.o
+echo running regtest
+./runUnittests_ldc
