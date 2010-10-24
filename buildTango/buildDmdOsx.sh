67082b89248d14d02f08b19e1d2246e641b1a3c0
Fawzi Mohamed <fawzi@gmx.ch>
2010-10-25 00:28:53 +0200

    * add suspendOneAtTime to flags

--- buildTango/buildDmdOsx.sh
+++ buildTango/buildDmdOsx.sh
@@ -3,8 +3,8 @@ TANGO_HOME=../../tango
 if [ $# -gt 0 ]; then
     TANGO_HOME=$1
 fi
-$TANGO_HOME/build/bin/osx32/bob -r=dmd -c=dmd -p=osx -l=libtango-base-dmd -o="-O -release -inline"  $TANGO_HOME
-$TANGO_HOME/build/bin/osx32/bob -u -c=dmd -p=osx -l=libtango-user-dmd -o="-g -O -release -inline"  $TANGO_HOME
+$TANGO_HOME/build/bin/osx32/bob -r=dmd -c=dmd -p=osx -l=libtango-base-dmd -o="-O -release -inline -version=SuspendOneAtTime"  $TANGO_HOME
+$TANGO_HOME/build/bin/osx32/bob -u -c=dmd -p=osx -l=libtango-user-dmd -o="-g -O -release -inline -version=SuspendOneAtTime"  $TANGO_HOME
 $TANGO_HOME/build/bin/osx32/bob -u -c=dmd -p=osx -l=libtango-user-dmd-dbg -o="-g -O -inline"  $TANGO_HOME
 $TANGO_HOME/build/bin/osx32/bob -u -c=dmd -p=osx -l=libtango-user-dmd-tst -o="-d -version=UnitTest -debug=UnitTest -unittest -g -debug -L-ltango-dbg"  $TANGO_HOME
 rm *.o
