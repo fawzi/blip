ddec5ea43f8ad26e6e8dd1cb88bc8b654fc0994c
Fawzi Mohamed <fawzi@gmx.ch>
2011-02-01 18:06:59 +0100

    * removing non useful inline flag

--- buildTango/buildLdc.sh
+++ buildTango/buildLdc.sh
@@ -3,9 +3,9 @@ TANGO_HOME=../../tango
 if [ $# -gt 0 ]; then
     TANGO_HOME=$1
 fi
-$TANGO_HOME/build/bin/linux64/bob -v -r=ldc -c=ldc -p=linux -l=libtango-base-ldc -o="-O3 -release -inline"  $TANGO_HOME
-$TANGO_HOME/build/bin/linux64/bob -v -u -c=ldc -p=linux -l=libtango-user-ldc -o="-g -O3 -release -inline"  $TANGO_HOME
-$TANGO_HOME/build/bin/linux64/bob -v -u -c=ldc -p=linux -l=libtango-user-ldc-dbg -o="-g -O -inline"  $TANGO_HOME
+$TANGO_HOME/build/bin/linux64/bob -v -r=ldc -c=ldc -p=linux -l=libtango-base-ldc -o="-O3 -release"  $TANGO_HOME
+$TANGO_HOME/build/bin/linux64/bob -v -u -c=ldc -p=linux -l=libtango-user-ldc -o="-O3 -release"  $TANGO_HOME
+$TANGO_HOME/build/bin/linux64/bob -v -u -c=ldc -p=linux -l=libtango-user-ldc-dbg -o="-g -O"  $TANGO_HOME
 $TANGO_HOME/build/bin/linux64/bob -v -u -c=ldc -p=linux -l=libtango-user-ldc-tst -o="-d -d-version=UnitTest -d-debug=UnitTest -unittest -g -L-ltango-dbg"  $TANGO_HOME
 rm *.o
 dirldc="`which ldc`"
@@ -13,7 +13,7 @@ dirldc=`dirname "$dirldc"`
 mv libtango-base-ldc.a libtango-user-ldc*.a "$dirldc/../lib"
 echo built and installed libs
 echo creating regtest
-ldc -d -d-version=UnitTest -d-debug=UnitTest -unittest -g -L-ltango-user-ldc-tst -L-lev -L-lz -L-lbz2 runUnittests_ldc.d
+ldc -d -d-version=UnitTest -d-debug=UnitTest -unittest -g -L-ltango-user-ldc-tst -L-lz -L-lbz2 runUnittests_ldc.d
 rm *.o
 echo running regtest
 ./runUnittests_ldc
