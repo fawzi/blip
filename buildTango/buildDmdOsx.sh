98d95c79eb8e788ded52346fcc13944bb88872c0
Fawzi Mohamed <fawzi@gmx.ch>
2011-02-25 16:22:30 +0100

    * different osx build sequence to better cope with broken bob versions

--- buildTango/buildDmdOsx.sh
+++ buildTango/buildDmdOsx.sh
@@ -3,16 +3,19 @@ TANGO_HOME=../../tango
 if [ $# -gt 0 ]; then
     TANGO_HOME=$1
 fi
-$TANGO_HOME/build/bin/osx32/bob -r=dmd -c=dmd -p=osx -l=libtango-base-dmd -o="-O -release -inline -version=SuspendOneAtTime"  $TANGO_HOME
-$TANGO_HOME/build/bin/osx32/bob -u -c=dmd -p=osx -l=libtango-user-dmd -o="-g -O -release -inline -version=SuspendOneAtTime"  $TANGO_HOME
-$TANGO_HOME/build/bin/osx32/bob -u -c=dmd -p=osx -l=libtango-user-dmd-dbg -o="-g -O -inline"  $TANGO_HOME
-$TANGO_HOME/build/bin/osx32/bob -u -c=dmd -p=osx -l=libtango-user-dmd-tst -o="-d -version=UnitTest -debug=UnitTest -unittest -g -debug -L-ltango-dbg"  $TANGO_HOME
+pushd $TANGO_HOME
+./build/bin/osx32/bob -r=dmd -c=dmd -p=osx -l=libtango-base-dmd -o="-O -release -inline -version=SuspendOneAtTime"  .
+./build/bin/osx32/bob -u -c=dmd -p=osx -l=libtango-user-dmd -o="-g -O -release -inline -version=SuspendOneAtTime"  .
+./build/bin/osx32/bob -u -c=dmd -p=osx -l=libtango-user-dmd-dbg -o="-g -O -inline"  .
+./build/bin/osx32/bob -u -c=dmd -p=osx -l=libtango-user-dmd-tst -o="-d -version=UnitTest -debug=UnitTest -unittest -g -debug -L-ltango-dbg"  .
 rm *.o
 dirDmd="`which dmd`"
 dirDmd=`dirname "$dirDmd"`
 mv libtango-base-dmd.a libtango-user-dmd*.a "$dirDmd/../lib"
 echo built and installed libs
+popd
 echo creating regtest...
+rm runUnittests_dmd
 dmd -d -version=UnitTest -debug=UnitTest -unittest -g -debug -L-ltango-user-dmd-tst -L-lz -L-lbz2 runUnittests_dmd.d
 rm *.o
 ./runUnittests_dmd
