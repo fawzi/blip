6dbf2a532df4017037c7b454989a9d40ad5454fa
Fawzi Mohamed <fawzi.mohamed@fhi-berlin.mpg.de>
2018-03-23 14:22:15 +0100

    disabling fixed fractionals

--- blip/omg/core/Fixed.d
+++ blip/omg/core/Fixed.d
@@ -49,9 +49,9 @@ struct fixed32T(int fracBits_) {
     
     static ClassMetaInfo metaI;
     static this(){
-        metaI=ClassMetaInfo.createForType!(typeof(this))("fixed32T!("~ctfe_i2a(fracBits)~")","a fractional number with a fixed precision");
+      /*metaI=ClassMetaInfo.createForType!(typeof(this))("fixed32T!("~ctfe_i2a(fracBits)~")","a fractional number with a fixed precision");
         metaI.addFieldOfType!(int)("int","integer part");
-        metaI.addFieldOfType!(int)("frac","fractional part");
+        metaI.addFieldOfType!(int)("frac","fractional part");*/
     }
     ClassMetaInfo getSerializationMetaInfo(){
         return metaI;
